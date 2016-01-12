ed22faab16541cb5eac435ec36fc9f80656af483
eddieduro <student@epicodus.com>
2016-01-12 11:59:59 -0800

    submit button checks for errors before submitting form

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+$("doccument").ready(function() {
+  // initially hide .errors when page loads
+  $('.error').hide();
+
+  $("#blank form").submit(function(event) {
+    // Hide errors after button is clicked
+    $('.error').hide();
+      var firstName = $("input#firstName").val();
+        if (firstName == "") {
+          $("label#firstName_error").show();
+          $("input#firstName").focus();
+          return false;
+        }
+      var lastName = $("input#lastName").val();
+        if (lastName == "") {
+          $("label#lastName_error").show();
+          $("input#lastName").focus();
+          return false;
+        }
+      var email = $("input#email").val();
+        if (email == "") {
+          $("label#email_error").show();
+          $("input#email").focus();
+          return false;
+        }
+      var date = $("input#date").val();
+        if (date == "") {
+          $("label#date_error").show();
+          $("input#date").focus();
+          return false;
+        }
+      var time = $("input#time").val();
+        if (time == "") {
+          $("label#time_error").show();
+          $("input#time").focus();
+          return false;
+        }
+      event.preventDefault();
+  });
+});
